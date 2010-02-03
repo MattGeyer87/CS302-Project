90dc29dfe7ce87e2249604c152b7beb6b1e159de
Matt <bwishardcore@gmail.com>
2010-02-02 17:46:11 -0800

    Test.

--- test.cpp
+++ test.cpp
@@ -4,7 +4,5 @@ using namespace std;
 int main(){
 
     cout << "This is a test." << endl;
-    cout << "Added this line, lol." << endl;
-    cout << "Added another line, hehe. " << endl;
     return 0;
 }
